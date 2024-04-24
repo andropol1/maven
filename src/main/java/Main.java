264f556fb5be2df3926a1d689ce8b3daa890b800
andropol1 <andrew.kondratenkov@gmail.com>
2024-04-24 23:52:41 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Game game = new Game(5);
+        game.pickDoor();
+        game.revealGoats();
+        game.printDoors();
+        game.changePick();
+        game.checkDoor();
+
+    }
+}
